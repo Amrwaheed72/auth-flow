51d69675281207a45ec3a4996dbafa8231a374b0
Amr Waheed <amrmohamed877@gmail.com>
2025-10-05 15:42:15 +0300

    fix a bug

--- app/main/dashboard/page.tsx
+++ app/main/dashboard/page.tsx
@@ -1,17 +1,16 @@
-// 'use client';
+'use client';
 
+const Page = () => {
+    // const userData = localStorage.getItem('UserData');
+    // const user = userData ? JSON.parse(userData) : null;
+    // const username = user.name.split(' ', 1);
+    // console.log(user);
+    // useEffect(() => {
+    //     if (!token) {
+    //         router.push('/auth/login');
+    //     }
+    // }, []);
+    return <div>hello </div>;
+};
 
-// const Page = () => {
-//     const userData = localStorage.getItem('UserData');
-//     const user = userData ? JSON.parse(userData) : null;
-//     const username = user.name.split(' ', 1);
-//     console.log(user);
-//     // useEffect(() => {
-//     //     if (!token) {
-//     //         router.push('/auth/login');
-//     //     }
-//     // }, []);
-//     return <div>Welcome {user && username} </div>;
-// };
-
-// export default Page;
+export default Page;
